111c606113f433ee4dc1b84da0dabdac86d7064b
imranimranr <139095710+imranimranr@users.noreply.github.com>
2023-07-14 10:21:18 +0530

    Update Constructor.java

--- ctcday1/src/day4/Constructor.java
+++ ctcday1/src/day4/Constructor.java
@@ -1,5 +1,16 @@
 package day4;
 
 public class Constructor {
-
+  void print() {
+		System.out.println("Normal method");
+	}
+	//default constructor
+	public Constructor_class() {
+		System.out.println("Default Constructor");
+	}
+	//Parameterized constructor
+	public Constructor_class(int a) {
+		System.out.println("Parameterized Constructor");
+		System.out.println(a);
+	}
 }
